52edef7d5d5836cd1abe40b6b81baebb7b3f44e4
Tim Beyer <tbeyer@hashicorp.com>
2024-06-27 07:49:29 -0700

    init

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.55.0"
+    }
+    hcp = {
+      source = "hashicorp/hcp"
+      version = ">= 0.93.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "hcp" {}
